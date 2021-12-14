22a9ca04c52b31658ebfa06c56bce110e3222e12
Nil Ballús <nballuri7@alumnes.ub.edu>
2021-12-14 14:16:16 +0100

    trial

--- models/baseline.py
+++ models/baseline.py
@@ -1,4 +1,7 @@
 from pytorch_lightning.core.lightning import LightningModule
+import torch
+import torch.nn as nn
+
 from models import ResNet18
 
 
@@ -7,6 +10,9 @@ class Baseline(LightningModule):
         super().__init__()
 
         self.model = ResNet18()
+        self.criterion = nn.CrossEntropyLoss()
+        self.learning_rate = 0.1
+        self.scheduler_length = 200
 
     def forward(self, x):
         return self.model(x)
@@ -19,3 +25,8 @@ class Baseline(LightningModule):
         #computar els que estan be i tambe fer un log
         #posar tant loss com accuracy en un array
         return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.scheduler_length)
+        return [optimizer], [scheduler]
